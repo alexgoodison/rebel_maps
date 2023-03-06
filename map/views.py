1ee4032fde2fc65f394c0c6230d19bcd12e76859
alexgoodison <alexandergoodison@gmail.com>
2023-03-06 13:35:23 +0000

    Added Map view with simple route entry

--- map/views.py
+++ map/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def map(request):
-  return HttpResponse("Map page")
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
